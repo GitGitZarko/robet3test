cbeb3999aa15de22f1dbbf53309b667d572d2400
zarkodragovic <zarkodragovic@gmail.com>
2019-06-03 17:11:59 +0200

    Work 03 May 2019 End2

--- src/reducers/changeOddValueReducer.js
+++ src/reducers/changeOddValueReducer.js
@@ -1,7 +1,10 @@
 import { CHANGE_ODD_VALUE } from '../actions';
-const oddTypeValue = JSON.parse(localStorage.OddType);
+const initialState = {
+    oddTypeValue: JSON.parse(localStorage.OddType)
+}
 
-export default (state = oddTypeValue, action) => {
+
+export default (state = initialState, action) => {
     switch(action.type){
         case CHANGE_ODD_VALUE:
             localStorage.setItem('OddType', JSON.stringify(action.payload));
