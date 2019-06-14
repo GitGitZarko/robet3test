a46f7303485e186cd335a65d9c62e55511cc4be8
GitGitZarko <zarkodragovic@gmail.com>
2019-06-14 17:23:52 +0200

    Work from home 14 June 2019

--- src/components/Casino/Game.js
+++ src/components/Casino/Game.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 //import '../public/css/Sports.css';
 
 const Game = ({ game }) => {
-	console.log(game)
+
+	const [displayButton, setDisplayButton] = useState(0);
+
 	return (
 		// <div className="eight wide mobile five wide computer center aligned column  ">
 		<div className="ui eight wide mobile four wide computer center aligned column">
+
 			<div className="ui image" style={{ border: '4px white solid' }}>
-				<img src={!game.iconUrl ? `/images/itcasino/${game.providerGameId}.png` : game.iconUrl} alt="Local Picrure" />
+
+				<img onMouseOver={() => setDisplayButton(1)}
+					onMouseOut={() => setDisplayButton(0)}
+					style={{ zIndex: 1 }}
+					src={!game.iconUrl ? `/images/itcasino/${game.providerGameId}.png` : game.iconUrl} alt="Local Picrure" />
+				<button className="huge ui yellow button"
+					style={{
+						position: 'relative',
+						top: '50%',
+
+						zIndex: 5,
+						opacity: displayButton
+					}}>
+					GIOCA
+				</button>
 				<div style={{ background: 'yellow' }}>
 					<h5>{game.gameName}</h5>
 				</div>
