cfc05a775d6302020a95f9bd9a47008b48045d8e
Zarko <zarkodragovic@gmail.com>
2019-08-02 11:37:48 +0200

    Fixed response.json problem

--- src/components/CmsPage.js
+++ src/components/CmsPage.js
@@ -11,15 +11,49 @@ export default class CmsPage extends Component {
     }
 
     async componentDidMount(){
-        console.log(data, this.props.match.params.CmsPage);
-        const url = `http://www.betvip.fun/CmsReactPages?pagename=${this.props.match.params.CmsPage}`;
-        const response = await fetch(url);
-        const data = await response.json();
-        const script = document.createElement("script");
-        this.setState({html: data.HtmlContent, script: data.Scripts})
-        console.log(data, this.props.match.params.CmsPage);
+        this.fetchCmsPages();
+        
         // const script = document.getElementById(match.params.customer).innerHTML;
         // window.eval(script);
+    }
+    ShowServerError = ({ error }) =>  {
+        if (!error) {
+          return null;
+        }
+        return (
+          <div className="alert">
+            <h3>Server Error</h3>
+            {error instanceof window.Response ? (
+              <p>
+                <b>{error.status}</b> on <b>{error.url}</b>
+                <br />
+                <small>{error.statusText}</small>
+              </p>
+            ) : (
+              <p>
+                <code>{error.toString()}</code>
+              </p>
+            )}
+          </div>
+        );
+      }
+    fetchCmsPages = async () => {
+        const url = `http://www.betvip.fun/CmsReactPages?pagename=${this.props.match.params.CmsPage}`;      
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+              this.setServerError(response);
+            }
+            const data = await response.json();
+            const script = document.createElement("script");
+            this.setState({html: data.HtmlContent, script: data.Scripts})
+            // do something here with `await response.json()`
+          } catch (ex) {
+            console.log("This is React SAPage.")
+          }
+
+        
+        
     }
 
 render(){ 
